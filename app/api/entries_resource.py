45fb4854c38fa370d5887f85d1c5eb0d2b00afff
Samuel Bwambale <simplesam63@gmail.com>
2018-07-24 13:32:03 +0300

    [Feature #159211031] add entries resource

---
+++ app/api/entries_resource.py
@@ -0,0 +1,102 @@
+from flask_restplus import Resource, reqparse
+from flask import jsonify, make_response
+
+parser = reqparse.RequestParser()
+parser.add_argument('title', type=str, required=True,
+                    help='Title must be a valid string')
+parser.add_argument('description', type=str, required=True,
+                    help='Description must be a valid string')
+
+entries_list = []
+
+class EntryResource(Resource):
+    def get(self, entry_id):
+        for entry in entries_list:
+            if entry['entry_id'] == entry_id:
+                return make_response(jsonify({
+                'status': 'success',
+                'entry': entry
+                }), 200)
+        return make_response(jsonify({
+                'status': 'failed',
+                'message': 'Entry with this ID not found.',
+                }), 404)            
+
+
+    def put(self, entry_id):
+        data = parser.parse_args()
+        for entry in entries_list:
+            if entry['entry_id'] == entry_id:
+                entry["title"] = data['title']
+                entry["description"] = data['description']
+                return make_response(jsonify({
+                    'status': 'success',
+                    'message': 'Entry edited successfully.',
+                    }), 200)
+        return make_response(jsonify({
+                'status': 'failed',
+                'message': 'Entry with this ID not found.',
+                }), 404)       
+
+            
+    def delete(self, entry_id):
+        for entry in entries_list:
+            if entry['entry_id'] == entry_id:
+                entries_list.remove(entry)
+                return make_response(jsonify({
+                'status': 'success',
+                'message': 'Entry deleted.'
+                }), 200)
+        return make_response(jsonify({
+                'status': 'failed',
+                'message': 'Entry with this ID not found.',
+                }), 404)
+    
+
+class EntryListResource(Resource):
+    def get(self):
+        return make_response(jsonify({'entries': entries_list}), 200)
+
+    def post(self):
+        data = parser.parse_args()
+        title = data['title']
+        description = data['description']
+        if title.strip() == "":
+            return make_response(jsonify({
+                'status': 'failed',
+                'message': 'Title can not be empty.'}), 400)
+
+        if len(title.strip()) < 4:
+            return make_response(jsonify({
+                'status': 'failed',
+                'message': 'Title should be at least 4 characters long.'}), 400)
+            
+        if description.strip() == "":
+            return make_response(jsonify({
+                'status': 'failed',
+                'message': 'Description can not be empty.'}), 400)
+
+        if len(description.strip()) < 6:
+            return make_response(jsonify({
+                'status': 'failed',
+                'message': 'Sescription should be at least 6 characters long.'}), 400)
+        for entry in entries_list:
+            if entry['title'] == title:
+                return make_response(jsonify({
+                    'status': "Failed",
+                    'message': 'Entry with same title already exists!',
+                    }), 400)
+        entry = {
+                "entry_id":len(entries_list)+1,
+                "title": title,
+                "description": description
+                    }
+        try:
+            entries_list.append(entry)
+            return make_response(jsonify({
+                'status': "success",
+                'message': 'Entry successfully created!',
+                }), 201)
+        except Exception as err:
+            return {'message': '{}'.format(err)}, 500
+            
