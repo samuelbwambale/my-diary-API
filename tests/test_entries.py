ddd49c930a9a90f3e1ffda003c04811dd1dd9211
Samuel Bwambale <simplesam63@gmail.com>
2018-07-25 03:25:12 +0300

    Add test to edit an entry

--- tests/test_entries.py
+++ tests/test_entries.py
@@ -36,7 +36,7 @@ class EntriesApiTestCase(unittest.TestCase):
         self.assertEqual(response.status_code, 400)
 
 
-    def test_add_entry_with_empty_fields(self):
+    def test_add_entry_with_empty_title(self):
         entry1 = {
             'title': '',
             'description': 'I love cycling'
@@ -44,6 +44,24 @@ class EntriesApiTestCase(unittest.TestCase):
         response = self.app.post("/api/v1/entries",\
         data=json.dumps(entry1), content_type='application/json')
         self.assertEqual(response.status_code, 400)
+
+    def test_add_entry_with_empty_description(self):
+        entry1 = {
+            'title': 'Rowing',
+            'description': ''
+        }
+        response = self.app.post("/api/v1/entries",\
+        data=json.dumps(entry1), content_type='application/json')
+        self.assertEqual(response.status_code, 400)
+
+    def test_add_entry_with_missing_fields(self):
+        entry1 = {
+            'description': 'This night'
+        }
+        response = self.app.post("/api/v1/entries",\
+        data=json.dumps(entry1), content_type='application/json')
+        self.assertEqual(response.status_code, 400)
+
 
     def test_add_entry_with_title_less_than_4_characters(self):
         entry = {
@@ -54,6 +72,7 @@ class EntriesApiTestCase(unittest.TestCase):
         data=json.dumps(entry), content_type='application/json')
         self.assertEqual(response.status_code, 400)
 
+
     def test_add_entry_with_description_less_than_6_characters(self):
         entry = {
             'title': ' Going to church',
@@ -63,6 +82,34 @@ class EntriesApiTestCase(unittest.TestCase):
         data=json.dumps(entry), content_type='application/json')
         self.assertEqual(response.status_code, 400)
 
+    
+    def test_edit_entry(self):
+        entry = {
+            'title': 'Swimming',
+            'description': 'In Munyonyo'
+        }
+        self.app.post("/api/v1/entries",\
+        data=json.dumps(entry), content_type='application/json')
+        new_entry = {
+            'title': 'Hunting',
+            'description': 'In the park'
+        }        
+        response = self.app.put("/api/v1/entries/2",\
+        data=json.dumps(new_entry), content_type='application/json')
+        self.assertEqual(response.status_code, 200)       
+
+
+    def test_get_an_entry(self):
+        entry = {
+            'title': ' Visit a Cinema',
+            'description': 'Go to Cineplex'
+        }
+        self.app.post("/api/v1/entries",\
+        data=json.dumps(entry), content_type='application/json')
+        response = self.app.get('/api/v1/entries/2', content_type = 'application/json')
+        self.assertEqual(response.status_code, 200)
+
+    
     def test_delete_entry(self):  
         entry = {
             'title': ' Live band',
@@ -72,4 +119,8 @@ class EntriesApiTestCase(unittest.TestCase):
         data=json.dumps(entry), content_type='application/json')      
         response = self.app.delete('/api/v1/entries/1', content_type = 'application/json')
         self.assertEqual(response.status_code, 200)
+
+
+
+
         
