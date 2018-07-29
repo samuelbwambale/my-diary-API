bac059f57a5ba9b415ffd649856d9bd61f17d017
Samuel Bwambale <simplesam63@gmail.com>
2018-07-29 14:46:47 +0300

    Change path to resources files after adding resources folder

--- app/api/__init__.py
+++ app/api/__init__.py
@@ -5,15 +5,14 @@ apiv1 = Blueprint('api',__name__)
 api = Api(apiv1)
 
 
-from .entries_resource import EntryListResource
-from .entries_resource import EntryResource
+from .resources.entries_resource import EntryListResource
+from .resources.entries_resource import EntryResource
 api.add_resource(EntryListResource,'/entries')
 api.add_resource(EntryResource,'/entries/<int:entry_id>')
 
-from .users_resource import UserListResource
-from .users_resource import UserRegister
-from .users_resource import UserLogin
+from .resources.users_resource import UserListResource
+from .resources.users_resource import UserRegister
+from .resources.users_resource import UserLogin
 api.add_resource(UserListResource,'/users')
 api.add_resource(UserRegister,'/auth/signup')
 api.add_resource(UserLogin,'/auth/login')
-
