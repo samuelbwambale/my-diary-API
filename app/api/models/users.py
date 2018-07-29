e967bb576d0ca6c5ee74748cf4b58af7b2b821f3
Samuel Bwambale <simplesam63@gmail.com>
2018-07-29 22:09:03 +0300

    [Feature #159385746] Modify user class

--- app/api/models/users.py
+++ app/api/models/users.py
@@ -1,60 +1,26 @@
 from werkzeug.security import generate_password_hash
-import uuid
-
+from api.database import Database
 
 class User:
+    connection = Database()
+    cursor = connection.cursor
+    dict_cursor = connection.dict_cursor
+
     def __init__(self, first_name, last_name, email, password):
-        self.id = uuid.uuid4().hex
         self.first_name = first_name
         self.last_name = last_name
         self.email = email
         self.password = generate_password_hash(password, method='sha256')
 
 
-    def add_user(self):
-        USERS.append(self)
-
-
-    def update_user(self, first_name, last_name, email, password):
-        self.first_name = first_name
-        self.last_name = last_name
-        self.email = email
-        self.password = password
-
-
-    def delete_user(self):
-        USERS.remove(self)
-
-
-    def json(self):
-        return {
-            'user_id': self.user_id,
-            'first_name': self.first_name,
-            'last_name': self.last_name,
-            'email': self.email,
-            'password': self.password
-       }
-
-
-def get_user_by_id(user_id):
-    for user in USERS:
-        if user['user_id'] == user_id: 
-            return user
-
-
-def get_user_by_email(email):
-    for user in USERS:
-        if user['email'] == email: 
-            return user
-
     
-def get_all_users():
-    return [user.json() for user in USERS]
-
-
-def check_if_email_exists(email):
-    for user in USERS:
-        if user.email == email:
-            return user.email
-            
     
+
+    def get_user_by_email(self):
+        try:
+            query = "SELECT * FROM users WHERE email = %s "
+            User.dict_cursor.execute(query, [self.email])
+            usr = User.dict_cursor.fetchone()
+            return usr
+        except psycopg2.Error as e:
+            print(e.pgerror)
