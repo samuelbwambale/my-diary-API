bf430dba26ca826918ecc69a37fca4676637ea47
Samuel Bwambale <simplesam63@gmail.com>
2018-07-30 12:26:28 +0300

    Move SQL queries to database class out of the models

--- app/api/models/entries.py
+++ app/api/models/entries.py
@@ -9,12 +9,3 @@ class Entry:
         self.description = description
         self.create_date = datetime.datetime.utcnow()
         self.user_id = user_id
-
-
-    def add_entry(self):
-        query = "INSERT INTO entries (title, description, create_date, user_id) VALUES (%s, %s, %s)"
-        User.cursor.execute(query,(
-            self.title, 
-            self.description, 
-            self.create_date, 
-            self.user_id))
