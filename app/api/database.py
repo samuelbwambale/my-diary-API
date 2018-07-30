7312ed9a9bd086a90a473ff03fc5b08ce4ec0fc8
Samuel Bwambale <simplesam63@gmail.com>
2018-07-30 17:40:59 +0300

    [Feature #156978991] Move SQL queries to the models as class methods

--- app/api/database.py
+++ app/api/database.py
@@ -7,12 +7,7 @@ class DatabaseConnection(object):
 
     def __init__(self):
         try:
-            self.connection = psycopg2.connect(
-                dbname = 'mydiarydb'
-                user = 'postgres'
-                password = 'postgres'
-                host =  '127.0.0.1'
-                port = '5432')
+            self.connection = psycopg2.connect("dbname = 'mydiarydb' user = 'postgres' password = 'postgres' host =  '127.0.0.1' port = '5432'")
             self.connection.autocommit = True
             self.cursor = self.connection.cursor()
             print("Connection successfull")
@@ -28,17 +23,18 @@ class DatabaseConnection(object):
         self.cursor.execute(create_table_users_query)
     
     def create_table_entries(self):
-        create_table_entries_query = ("CREATE TABLE IF NOT EXISTS entries (entry_id SERIAL PRIMARY KEY, \
+        create_table_entries_query = ("CREATE TABLE IF NOT EXISTS entries \
+        (entry_id SERIAL PRIMARY KEY, \
         owner_id INTEGER NOT NULL, \
         title VARCHAR(125) NOT NULL,\
         description VARCHAR(255) NOT NULL,\
         create_date timestamp, \
-        FOREIGN KEY (owner_id) REFERENCES users (user_id)\
+        FOREIGN KEY (user_id) REFERENCES users (user_id)\
         ON UPDATE CASCADE ON DELETE CASCADE)")
         self.cursor.execute(create_table_entries_query)
 
 
-    def add_user(self, first_name, last_name, email, password):
+    def register_user(self, first_name, last_name, email, password):
         try:
             query = "INSERT INTO users (first_name, last_name, email, password) VALUES (%s, %s, %s)"
             self.cursor.execute(query,(first_name, last_name, email, password))
@@ -53,7 +49,7 @@ class DatabaseConnection(object):
             self.cursor = self.connection.cursor(cursor_factory=ex.DictCursor)
             query = "SELECT * FROM users WHERE email='"+email+"' "
             result = self.cursor.fetchone()
-            if check_password_hash(result['password'], password)
+            if check_password_hash(result['password'], password):
                 return result
             else:
                 return False
@@ -61,13 +57,14 @@ class DatabaseConnection(object):
             print(e.pgerror)
 
 
+
     def get_all_users(self):
 		try:
 			self.cursor=self.connection.cursor(cursor_factory=ex.DictCursor)
-			query = "SELECT * FROM users "
+			query = "SELECT * FROM users"
             self.cursor.execute(query)
 			result = self.cursor.fetchall()
-			if  result != [] :				
+			if  result != []:				
 				return result
 			else:
 			 	return False
