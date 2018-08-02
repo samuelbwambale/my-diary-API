d117cf00afc385186a16b18a08008f4d3b837b7b
Samuel Bwambale <simplesam63@gmail.com>
2018-08-02 05:53:32 +0300

    Change role from postgres to root

--- app/api/database.py
+++ app/api/database.py
@@ -1,33 +1,30 @@
-from werkzeug.security import check_password_hash
 import psycopg2
-
 
 class DatabaseConnection:
 
     def __init__(self):
         try:
-            self.connection = psycopg2.connect("dbname = 'mydiarydb' user = 'postgres' password = 'postgres' host =  '127.0.0.1' port = '5432'")
+            self.connection = psycopg2.connect("dbname='mydiarydb' user='root' password='root' host='localhost'")
             self.connection.autocommit = True
             self.cursor = self.connection.cursor()
-            print("Connection successfull")
-        except:
-            print("Connection failed.")
+            print("Connection successful")
+        except psycopg2.Error as e:
+            print(e.pgerror)
 
 
     def create_table_users(self):
-        create_table_users_query = ("CREATE TABLE IF NOT EXISTS users \
-        (user_id SERIAL PRIMARY KEY, first_name VARCHAR(25) NOT NULL, \
-        last_name VARCHAR(25) NOT NULL, email VARCHAR(28) NOT NULL, \
-        password VARCHAR(12) NOT NULL)")
-        self.cursor.execute(create_table_users_query)
-    
+        try:
+            query = "CREATE TABLE IF NOT EXISTS users (user_id SERIAL PRIMARY KEY, first_name VARCHAR(25) NOT NULL, last_name VARCHAR(25) NOT NULL, email VARCHAR(25) NOT NULL, password VARCHAR(15) NOT NULL)"
+            self.cursor.execute(query)
+            print("Users table created")
+        except psycopg2.Error as e:
+            print(e.pgerror)
+            
+
     def create_table_entries(self):
-        create_table_entries_query = ("CREATE TABLE IF NOT EXISTS entries \
-        (entry_id SERIAL PRIMARY KEY, \
-        owner_id INTEGER NOT NULL, \
-        title VARCHAR(125) NOT NULL,\
-        description VARCHAR(255) NOT NULL,\
-        create_date timestamp, \
-        FOREIGN KEY (owner_id) REFERENCES users (user_id)\
-        ON UPDATE CASCADE ON DELETE CASCADE)")
-        self.cursor.execute(create_table_entries_query)
+        try:
+            query = "CREATE TABLE IF NOT EXISTS entries (entry_id SERIAL PRIMARY KEY, owner_id INTEGER NOT NULL, title VARCHAR(255) NOT NULL, description VARCHAR(255) NOT NULL, create_date timestamp, FOREIGN KEY (owner_id) REFERENCES users (user_id) ON UPDATE CASCADE ON DELETE CASCADE)"
+            self.cursor.execute(query)
+            print("Entries table created")
+        except psycopg2.Error as e:
+            print(e.pgerror)
