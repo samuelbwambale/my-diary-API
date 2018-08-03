5d480ee51a39f57c6e47c5a1bd194536e80e14ae
Samuel Bwambale <simplesam63@gmail.com>
2018-08-03 08:15:59 +0300

    Remove TestLoader function in app.py

--- app.py
+++ app.py
@@ -1,14 +1,5 @@
-from flask_script import Manager
 from app import app
-import unittest
-manager = Manager(app)
-
-@manager.command
-def test():
-    tests = unittest.TestLoader().discover('tests', pattern='test*.py')
-    unittest.TextTestRunner(verbosity=2).run(tests)
-    
 
 if __name__ == '__main__':
-    manager.run()
+    app.run()
     
