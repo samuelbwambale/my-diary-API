bacc8e7eed7c2230f6ab0667940995e0a417f686
Samuel Bwambale <simplesam63@gmail.com>
2018-08-03 09:14:33 +0300

    Add methods to drop tables in tearDown

--- tests/base.py
+++ tests/base.py
@@ -14,15 +14,17 @@ class BaseTestCase(unittest.TestCase):
         self.app = app.test_client()
         with app.test_request_context():
             database_connection = DatabaseConnection()
+            database_connection.drop_table_users()
+            database_connection.drop_table_entries()            
             database_connection.create_table_users()
             database_connection.create_table_entries()
-
-
             self.token = create_access_token(identity=1)
             self.header = {"Authorization" : "Bearer {}". format(self.token)}  
 
 
     def tearDown(self):
-        database_connection = DatabaseConnection()
-        database_connection.cursor.execute("DELETE FROM users")
-        database_connection.cursor.execute("DELETE FROM users")
+        with app.test_request_context():
+            database_connection = DatabaseConnection()
+            database_connection.drop_table_users()
+            database_connection.drop_table_entries()
+            database_connection.stop_connection()
