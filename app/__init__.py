2d07d02c4ecc2ae4e5b388cbb79e8c05491bd8f9
Samuel Bwambale <simplesam63@gmail.com>
2018-08-14 16:00:20 +0300

    Add CORS to the app

--- app/__init__.py
+++ app/__init__.py
@@ -1,11 +1,13 @@
 from flask import Flask
 from flask_jwt_extended import JWTManager
+from flask_cors import CORS
 from config import config
 
 
 app = Flask(__name__)
 """ Initialize the flask application """
-
+CORS(app)
+""" Enable cross-origin resource sharing """
 app.config.from_object(config['development'])
 jwt = JWTManager(app)
 app.config['SECRET_KEY'] = 'alibaba'
