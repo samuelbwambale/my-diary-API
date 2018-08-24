ba4105c140dc383508d76043ad3374f4a66a4c15
Samuel Bwambale <simplesam63@gmail.com>
2018-08-24 16:45:08 +0300

    Add Fetch API requests to backend

--- UI/js/entries.js
+++ UI/js/entries.js
@@ -1,4 +1,44 @@
-//document.getElementById("get_entries_button").addEventListener("submit", test_log_something)
+function changediv(){
+    let editable = document.getElementById('desc')
+    editable.contentEditable = "true"
+    let editbtn = document.getElementById('editbtn')
+    editbtn.innerText="Save";
+    editbtn.setAttribute("onclick","update_an_entry()")
+}
+
+
+function update_an_entry(){
+    let description = document.getElementById('desc').innerText;
+    let id = document.getElementById('entryid').innerText;
+    console.log(description)
+    
+    fetch('http://127.0.0.1:5000/api/v1/entries/'+id, {
+      method:'PUT',
+      headers: {
+        'Content-type':'application/json; charset=UTF-8',
+        'authorization':'Bearer '+sessionStorage.getItem('token')
+      },
+      body:JSON.stringify({
+        description:description})
+    })
+    .then(function(response) {
+        return response.json()
+      })
+    .then(function(data) {
+        if(data.message === "Entry edited successfully"){
+        alert(data.message)
+        window.location.href = "./entries.html"
+        } else if(data.message === "Internal Server Error"){
+          let res = "Session has expired, please login and try again"
+          alert(res)
+          window.location.href = "./login.html"        
+      } else{
+            document.getElementById('view_entries_response').innerHTML = "Error : " + data.message;
+        }        
+    })
+    .catch(error => console.log(error))
+}
+
 function get_entries(){
 
     fetch('http://127.0.0.1:5000/api/v1/entries', {
@@ -11,14 +51,14 @@ function get_entries(){
     .then(response => response.json())
     .then(function(data) {
         if(data.status === "success"){
-        alert(data.status)
-        let entries_output = '<h4 style = "color: rgb(73, 35, 35);" class="display-4 mb-4">My Entries</h4>'
+        let entries_output = '<h2 style = "color: #5f044b;" class="mb-4">My Entries</h4>'
         data.entries.forEach(entry => {
             entries_output += `
             <div class="entry-div">
-                <h2><a href="#" onclick="get_an_entry(${entry.entry_id})">Title: ${entry.title}</a></h2>
-                <p>ID: ${entry.entry_id}</p>
-                <p>Description: ${entry.description}</p>
+                <h3><a href="#" onclick="get_an_entry(${entry.entry_id})">${entry.title}</a>
+                </h3>
+                <p>${entry.description}</p>
+                <p class="mb-5">${entry.create_date}</p>
             <div>
             `
         })
@@ -38,7 +78,6 @@ function get_entries(){
 
 
 function get_an_entry(id){
-
     fetch('http://127.0.0.1:5000/api/v1/entries/'+id, {
       method:'GET',
       headers: {
@@ -47,18 +86,17 @@ function get_an_entry(id){
       }
     })
     .then(response => {
-        console.log(response)
         response.json().then(function(data) {
-            console.log(data)
-            alert(data.status)
             if(data.status === "success"){
-                let entries_output = '<h3 style = "color: rgb(73, 35, 35);" class="display-4 mb-4">Single Entry</h3>'
-                
-                entries_output += `
+                let entries_output = `
                 <div class="entry-div">
-                    <h2>Title: ${data.entry.title}</h2>
-                    <p>ID: ${data.entry.entry_id}</p>
-                    <p>Description: ${data.entry.description}</p>
+                    <h2 class="entry-title">${data.entry.title}</h2>
+                    <p id='entryid' style="display: none;" >${data.entry.entry_id}</p>
+                    <p id="desc" contenteditable="false" >${data.entry.description}</p>
+                    <button id="editbtn" class="btn-edit" onclick="changediv()">Edit</button>
+                    <button onclick="delete_an_entry(${data.entry.entry_id})" class="btn-delete-entry">Delete</button>
+                    <button class="btn-cancel" onclick="window.location='./entries.html'">Cancel</button>
+
                 <div>
                 ` 
                 document.getElementById('entries_output').innerHTML = entries_output
@@ -73,8 +111,33 @@ function get_an_entry(id){
         })
     })
     .catch(error => console.log(error))
-  }
+}
 
+function delete_an_entry(id){
+
+    fetch('http://127.0.0.1:5000/api/v1/entries/'+id, {
+      method:'DELETE',
+      headers: {
+        'Content-type':'application/json; charset=UTF-8',
+        'Authorization':'Bearer '+sessionStorage.getItem('token')
+      }
+    })
+    .then(response => {
+        response.json().then(function(data) {
+            if(data.message === "Selected entry has been deleted"){
+                alert(data.message)
+                window.location.href = "./entries.html"
+            } else if(data.message === "Internal Server Error"){
+                let res = "Session has expired, please login and try again"
+                alert(res)
+                window.location.href = "./login.html"        
+            } else{
+                    document.getElementById('view_entries_response').innerHTML = data.message
+                }
+        }) 
+    })   
+    .catch(error => console.log(error))
+}
 
 
 
